0f7d0a8b913bf1fcc379bbbae6beb71d0ef19944
brittneyq <brittneynguyen821@gmail.com>
2021-09-21 08:41:30 -0700

    LRCI-2462 Make regex even more specific

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/TestrayAttachmentRecorder.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/testray/TestrayAttachmentRecorder.java
@@ -539,7 +539,7 @@ public class TestrayAttachmentRecorder {
 						JenkinsResultsParserUtil.write(
 							poshiReportHTMLFile,
 							content.replaceAll(
-								"screenshot/[A-z]+[\\d]+.jpg", "\\.jpg\\.gz"));
+								"screenshots/[before|after]+[\\d]+.jpg", "\\.jpg\\.gz"));
 					}
 					catch (IOException ioException) {
 						throw new RuntimeException(ioException);
