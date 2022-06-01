710d2a0fa465124c0ddd6bf3811881ff00d6456b
Brittney Nguyen <brittneynguyen821@gmail.com>
2022-06-01 11:46:35 -0700

    LRCI-1398 Check if combined PQL contains test query

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/FunctionalBatchTestClassGroup.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/FunctionalBatchTestClassGroup.java
@@ -347,7 +347,9 @@ public class FunctionalBatchTestClassGroup extends BatchTestClassGroup {
 		System.out.println("******PQL 2 ***** :" + testBatchPropertyQuery);
 
 		if (!JenkinsResultsParserUtil.isNullOrEmpty(testBatchPropertyQuery) &&
-			!testBatchPropertyQuery.equals("false")) {
+			!testBatchPropertyQuery.equals("false") &&
+			!_combinedTestBatchRunPropertyQuery.contains(
+				testBatchPropertyQuery)) {
 
 			System.out.println("PQL IS NOT EMPTY!!!");
 
