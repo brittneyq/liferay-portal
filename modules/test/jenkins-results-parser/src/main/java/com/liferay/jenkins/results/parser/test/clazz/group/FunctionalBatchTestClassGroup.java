0c95e8eed6df9194922a5a40c653c5376df2b8fb
Brittney Nguyen <brittneynguyen821@gmail.com>
2022-06-08 14:20:23 -0700

    LRCI-1398 Comment relevant changes check

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/FunctionalBatchTestClassGroup.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/group/FunctionalBatchTestClassGroup.java
@@ -14,8 +14,6 @@
 
 package com.liferay.jenkins.results.parser.test.clazz.group;
 
-import com.google.common.collect.Lists;
-
 import com.liferay.jenkins.results.parser.AntException;
 import com.liferay.jenkins.results.parser.AntUtil;
 import com.liferay.jenkins.results.parser.JenkinsResultsParserUtil;
@@ -340,7 +338,7 @@ public class FunctionalBatchTestClassGroup extends BatchTestClassGroup {
 		return getDefaultTestBatchRunPropertyQuery(testBaseDir, testSuiteName);
 	}
 
-	private List<File> _getFunctionalRequiredModuleDirs(List<File> moduleDirs) {
+	/*private List<File> _getFunctionalRequiredModuleDirs(List<File> moduleDirs) {
 		List<File> functionalRequiredModuleDirs = Lists.newArrayList(
 			moduleDirs);
 
@@ -378,13 +376,13 @@ public class FunctionalBatchTestClassGroup extends BatchTestClassGroup {
 		}
 
 		return Lists.newArrayList(functionalRequiredModuleDirs);
-	}
+	}*/
 
 	private String _getTestBatchRunPropertyQuery(File testBaseDir) {
-		if (!testRelevantChanges) {
-			return getDefaultTestBatchRunPropertyQuery(
-				testBaseDir, testSuiteName);
-		}
+		//		if (!testRelevantChanges) {
+		//			return getDefaultTestBatchRunPropertyQuery(
+		//				testBaseDir, testSuiteName);
+		//		}
 
 		Set<File> modifiedFilesList = new HashSet<>();
 
