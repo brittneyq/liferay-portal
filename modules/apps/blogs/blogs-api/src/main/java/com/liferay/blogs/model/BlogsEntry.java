4ac18b73a6fc28b27bad166c289de665b2ed9b1a
Brittney Nguyen <brittneynguyen821@gmail.com>
2022-09-21 15:10:51 -0700

    LRCI-3229 Add nonposhi change

--- modules/apps/blogs/blogs-api/src/main/java/com/liferay/blogs/model/BlogsEntry.java
+++ modules/apps/blogs/blogs-api/src/main/java/com/liferay/blogs/model/BlogsEntry.java
@@ -23,7 +23,7 @@ import org.osgi.annotation.versioning.ProviderType;
 /**
  * The extended model interface for the BlogsEntry service. Represents a row in the &quot;BlogsEntry&quot; database table, with each column mapped to a property of this class.
  *
- * @author Brian Wing Shun Chan
+ * @author Brian Wing Shun Chans
  * @see BlogsEntryModel
  * @generated
  */
