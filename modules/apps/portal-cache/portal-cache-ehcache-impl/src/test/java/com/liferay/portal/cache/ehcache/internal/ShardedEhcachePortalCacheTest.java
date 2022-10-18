711ec63dcf3873d27527979cf00023486c779497
Brittney Nguyen <brittneynguyen821@gmail.com>
2022-10-18 14:51:29 -0700

    Revert "LPS-160370 Minimum tests to cover SYSTEM case"
    This reverts commit fd2f0e70100729080b23672ea7029ace6fd65bf5.

--- modules/apps/portal-cache/portal-cache-ehcache-impl/src/test/java/com/liferay/portal/cache/ehcache/internal/ShardedEhcachePortalCacheTest.java
+++ modules/apps/portal-cache/portal-cache-ehcache-impl/src/test/java/com/liferay/portal/cache/ehcache/internal/ShardedEhcachePortalCacheTest.java
@@ -18,7 +18,6 @@ import com.liferay.portal.cache.AggregatedPortalCacheListener;
 import com.liferay.portal.db.partition.DBPartitionUtil;
 import com.liferay.portal.kernel.cache.PortalCacheListener;
 import com.liferay.portal.kernel.cache.PortalCacheListenerScope;
-import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.test.ReflectionTestUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
@@ -110,10 +109,6 @@ public class ShardedEhcachePortalCacheTest {
 			new EhcachePortalCacheConfiguration(
 				_TEST_CACHE_NAME, Collections.emptySet(), false));
 
-		_companyIdThreadLocal.set(CompanyConstants.SYSTEM);
-
-		_shardedEhcachePortalCache.put(_TEST_KEY_SYSTEM, _TEST_VALUE_SYSTEM);
-
 		_companyIdThreadLocal.set(_TEST_COMPANY_ID_1);
 
 		_shardedEhcachePortalCache.put(_TEST_KEY_1, _TEST_VALUE_1);
@@ -195,15 +190,6 @@ public class ShardedEhcachePortalCacheTest {
 
 	@Test
 	public void testGet() {
-		_companyIdThreadLocal.set(CompanyConstants.SYSTEM);
-
-		Assert.assertSame(
-			_TEST_VALUE_1, _shardedEhcachePortalCache.get(_TEST_KEY_1));
-		Assert.assertNull(_shardedEhcachePortalCache.get(_TEST_KEY_2));
-		Assert.assertSame(
-			_TEST_VALUE_SYSTEM,
-			_shardedEhcachePortalCache.get(_TEST_KEY_SYSTEM));
-
 		_companyIdThreadLocal.set(_TEST_COMPANY_ID_1);
 
 		Assert.assertSame(
@@ -676,13 +662,9 @@ public class ShardedEhcachePortalCacheTest {
 
 	private static final String _TEST_KEY_2 = "TEST_KEY_2";
 
-	private static final String _TEST_KEY_SYSTEM = "TEST_KEY_SYSTEM";
-
 	private static final String _TEST_VALUE_1 = "TEST_VALUE_1";
 
 	private static final String _TEST_VALUE_2 = "TEST_VALUE_2";
-
-	private static final String _TEST_VALUE_SYSTEM = "TEST_VALUE_SYSTEM";
 
 	private static CacheManager _cacheManager;
 	private static ThreadLocal<Long> _companyIdThreadLocal;
