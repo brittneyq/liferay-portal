e632b6a6d7824eefc18b3ef6b9df240de4e5c67a
Brittney Nguyen <brittney.nguyen@liferay.com>
2023-11-15 15:29:03 -0800

    LRCI-3841 Add test

--- modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/upgrade/registry/BlogsServiceUpgradeStepRegistrator.java
+++ modules/apps/blogs/blogs-service/src/main/java/com/liferay/blogs/internal/upgrade/registry/BlogsServiceUpgradeStepRegistrator.java
@@ -39,7 +39,7 @@ import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
 /**
- * @author Adolfo Pérez
+ * @author Adolfo Pérezs
  */
 @Component(service = UpgradeStepRegistrator.class)
 public class BlogsServiceUpgradeStepRegistrator
