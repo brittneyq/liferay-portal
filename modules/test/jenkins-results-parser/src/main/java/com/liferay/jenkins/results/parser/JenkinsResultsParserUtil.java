7c574daf4e7f856c25436d76fd7efef3f0c72c8f
Brittney Nguyen <brittney.nguyen@liferay.com>
2023-11-22 08:07:39 -0800

    LRCI-3893 Get rid of print outs

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/JenkinsResultsParserUtil.java
@@ -184,20 +184,12 @@ public class JenkinsResultsParserUtil {
 					String.valueOf(content.length())));
 		}
 
-		System.out.println("IN APPEND FOR file : " + file);
-
-		System.out.println("CONTENT : " + content);
-
 		File parentDir = file.getParentFile();
-
-		System.out.println("PARENT DIR : " + parentDir);
 
 		if ((parentDir != null) && !parentDir.exists()) {
 			if (debug) {
 				System.out.println("Make parent directories for " + file);
 			}
-
-			System.out.println("PARENT DIR IS NOT NULL OR DOESNT EXIST");
 
 			parentDir.mkdirs();
 		}
@@ -205,8 +197,6 @@ public class JenkinsResultsParserUtil {
 		try (OutputStream outputStream = Files.newOutputStream(
 				Paths.get(file.toURI()), StandardOpenOption.CREATE,
 				StandardOpenOption.APPEND)) {
-
-			System.out.println("OUTPUT STREAM WRITE CONTENT TO BYTES");
 
 			outputStream.write(content.getBytes());
 		}
@@ -5508,10 +5498,8 @@ public class JenkinsResultsParserUtil {
 				"Write file " + file + " with length " + content.length());
 		}
 
-		System.out.println("WRITE in jrp : " + file);
-
 		if (file.exists()) {
-			System.out.println("FILE EXISTS... DELETING FILE");
+			System.out.println("FILE EXISTS... DELETING FILE : " + file);
 
 			file.delete();
 		}
