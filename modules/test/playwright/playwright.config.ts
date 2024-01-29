2b86a973ff62c1acd00d0ded63bdb07c03dbf0cf
Brittney Nguyen <brittney.nguyen@liferay.com>
2024-01-29 12:35:33 -0800

    LRCI-4040 Add playwright stable tests and configure

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -14,6 +14,7 @@ import {config as headlessBuilderWebConfig} from './tests/headless-builder-web/c
 import {config as layoutContentPageEditorWebConfig} from './tests/layout-content-page-editor-web/config';
 import {config as objectWebConfig} from './tests/object-web/config';
 import {config as portalWebConfig} from './tests/portal-web/config';
+import {config as stableConfig} from './tests/stable/config';
 import {config as usersAdminWebConfig} from './tests/users-admin-web/config';
 
 export default defineConfig({
@@ -31,6 +32,7 @@ export default defineConfig({
 		layoutContentPageEditorWebConfig,
 		objectWebConfig,
 		portalWebConfig,
+		stableConfig,
 		usersAdminWebConfig,
 	],
 	reporter: [
