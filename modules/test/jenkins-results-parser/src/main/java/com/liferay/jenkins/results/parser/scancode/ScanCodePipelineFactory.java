64303572bbae0ba34c0976f1d517d44942d6433b
Brittney Nguyen <brittney.nguyen@liferay.com>
2025-01-30 10:48:07 -0800

    LRCI-5172 Add pipeline factory

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/scancode/ScanCodePipelineFactory.java
@@ -0,0 +1,29 @@
+/**
+ * SPDX-FileCopyrightText: (c) 2024 Liferay, Inc. https://liferay.com
+ * SPDX-License-Identifier: LGPL-2.1-or-later OR LicenseRef-Liferay-DXP-EULA-2.0.0-2023-06
+ */
+
+package com.liferay.jenkins.results.parser.scancode;
+
+/**
+ * @author Brittney Nguyen
+ */
+public class ScanCodePipelineFactory {
+
+	public static ScanCodePipeline newPipeline(
+		String buildURL, String pipelineName) {
+
+		if (pipelineName.equals("analyze_docker_image")) {
+			return new AnalyzeDockerImagePipeline(buildURL, pipelineName);
+		}
+		else if (pipelineName.equals("inspect_packages")) {
+			return new InspectPackagesPipeline(buildURL, pipelineName);
+		}
+		else if (pipelineName.equals("map_deploy_to_develop")) {
+			return new MapDevelopToDeployPipeline(buildURL, pipelineName);
+		}
+
+		return null;
+	}
+
+}
